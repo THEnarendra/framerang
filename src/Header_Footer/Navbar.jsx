ccdcbee1439739b29007c994d2c7036f81399d65
THEnarendra <narendrak.kumawat123@gmail.com>
2024-04-30 15:11:59 +0530

    nav

--- src/Header_Footer/Navbar.jsx
+++ src/Header_Footer/Navbar.jsx
@@ -12,25 +12,25 @@ const Navbar = () => {
   return (
   <div >
         <Row className="navbar">
-          <Col lg={1} md={1} sm={12}>
+          <Col lg={1} md={3} sm={2}>
         <img
             className="image"
             src="https://framerang.com/cdn/shop/files/ll_copy.png?v=1712908551&width=135"
             alt=""
           />
           </Col>
-          <Col lg={3} md={10} sm={12}>
+          <Col lg={3} md={8} sm={8}>
             <input className="search" type="search" placeholder="Search Anime" />
           </Col>
-          <Col lg={1} md={12}>Home</Col>
-          <Col lg={1} md={12}>Anime List</Col>
-          <Col lg={2} md={12}>New Season</Col>
-          <Col lg={1} md={12}>Popular</Col>
-          <Col lg={2} md={12}>
+          <Col className="navMenu" lg={1} md={12}>Home</Col>
+          <Col className="navMenu" lg={1} md={12}>Frames</Col>
+          <Col className="navMenu" lg={1} md={12}>Posters</Col>
+          <Col className="navMenu" lg={1} md={12}>Track</Col>
+          <Col className="navMenu" lg={2} md={12}>
             <i class=" fa-solid fa-bell"></i>
             <i class="px-4 fa-solid fa-user"></i>
-            <i class=" fa-solid fa-bars"></i>
           </Col>
+          <Col className="burger"><i class=" fa-solid fa-bars"></i></Col>
         </Row>
       </div>
      
