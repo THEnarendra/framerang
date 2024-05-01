a45f3a57d4c777c4cd57e8e97f19c8286d16c7f7
Niranjan Sharma <ns350099@gmail.com>
2024-05-01 11:11:58 +0530

    kkk

--- src/App.js
+++ src/App.js
@@ -2,11 +2,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './Header_Footer/Navbar';
 import About from './About/About';
-<<<<<<< HEAD
 import Footer from './Header_Footer/Footer';
-=======
 import { Home } from './Home/Home';
->>>>>>> 938e8b2ba38edc2d30ead1e3941a0a7527fc86d8
 
 function App() {
   return (
