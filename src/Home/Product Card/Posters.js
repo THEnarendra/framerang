624546d3e6d38427181f5650c5bf3fcc9492758d
THEnarendra <narendrak.kumawat123@gmail.com>
2024-05-15 13:03:57 +0530

    product card

--- src/Home/Product Card/Posters.js
+++ src/Home/Product Card/Posters.js
@@ -1,17 +1,41 @@
 import React from 'react'
 import {ap10,ap11,ap12} from '../../images/anime-posters/animeposters'
+import Slider from 'react-slick'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import ProductCard from './ProductCard';
 
 export const Posters = ({theme}) => {
+
+  const settings = {
+    dots: true,
+    slidesToShow: 1,
+    infinite: true,
+    slidesToScroll: 1,
+    autoplay: true, // Add autoplay
+    speed: 2000,
+    autoplaySpeed: 2000, // Adjust autoplay speed (optional)
+    arrows: false, // Hide arrows
+  };
+
   return (
     <div>
+      
  <div style={{
-                marginTop:"132px",
+                marginTop:"72px",
                 padding: "3%", textAlign: "center", 
             }}>
               
-                
-            <img src={ap10} alt="" className='logo_wheel12' />
-            </div>
+              <Slider {...settings}>
+      <img src={ap10} alt="" className='logo_wheel12' />
+      <img src={ap10} alt="" className='logo_wheel12' />
+      </Slider>
+</div>
+
+{/* <h1 className='ms-5 display-4'>Anime Posters</h1> */}
+
+
+<ProductCard/>
 
     </div>
   )
--- src/Home/Product Card/ProductCard.jsx
+++ src/Home/Product Card/ProductCard.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
-import '../../MainCss/ProductCard.css'
+import img from '../../images/anime-character-listening-music.jpg'
 
 const ProductCard = () => {
   return (
-    <>
-    
-    </>
+    <div class="nft">
+    <div class='main'>
+<img src={img} alt="" />
+<h2 className='creator'>Luffy One Piec</h2>
+<p><span style={{textDecoration:"line-through"}}>Rs.50</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Rs.100</p>
+<button className='bt1'>Add to Cart</button>
+
+</div>
+</div>
   );
 }
 
