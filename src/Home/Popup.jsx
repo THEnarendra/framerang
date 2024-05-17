51d710f001b0438be9007622e99d91c349fea753
Niranjan Sharma <ns350099@gmail.com>
2024-05-17 15:32:08 +0530

    kk

--- src/Home/Popup.jsx
+++ src/Home/Popup.jsx
@@ -11,9 +11,16 @@ const Popup = () => {
         </Col>
 
         <Col lg={6}>
+          <span>Framerang</span>
           <h1>Anime Posters collection</h1>
-          <h3 className='creator'>{img.productName}</h3>( 10*5 inches ) <br /> Single pcs
-          <p><span style={{ textDecoration: "line-through", color: "gray" }}>Rs.{img.oldPrice}</span>&nbsp;&nbsp;&nbsp;<span style={{ fontSize: "22px" }}>Rs.{img.price}</span> </p>
+          <span>10*5 inches </span><br />
+          <span>Single pcs</span>
+          <br />
+        <span style={{ textDecoration: "line-through", color: "gray" }}>Rs. 50</span>&nbsp;&nbsp;&nbsp;<span style={{ fontSize: "22px" }}>Rs. 100</span>
+          <p>Size</p>
+          <button className='bt3'>A3</button>
+          <button className='bt3 ms-2'>A4</button><br />
+          
         </Col>
       </Row>
     </div>
