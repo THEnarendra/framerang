ca040a86267aa5b587a9ce80f2d8e05739fdfe2b
THEnarendra <narendrak.kumawat123@gmail.com>
2024-05-17 12:06:10 +0530

    popup

--- src/Home/Popup.jsx
+++ src/Home/Popup.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Col, Row } from 'react-bootstrap'
+import img from '../images/black-white-illustration-man-with-short-hair-black-white-face.jpg'
 
 const Popup = () => {
   return (
-    <div class="modal-dialog modal-dialog-scrollable">
-  ...
+    <div class="modal-dialog modal-dialog-scrollable" style={{position:"absolute", zIndex:999, width:"100vw"}}                                         >
+      <Row>
+        <Col lg={6}>
+    <img src={img} alt="" />
+        </Col>
+
+        <Col lg={6}>
+        <h1>Anime Posters collection</h1>
+        <h3 className='creator'>{img.productName}</h3>( 10*5 inches ) <br/> Single pcs 
+<p><span style={{textDecoration:"line-through",color:"gray"}}>Rs.{img.oldPrice}</span>&nbsp;&nbsp;&nbsp;<span style={{fontSize:"22px"}}>Rs.{img.price}</span> </p>
+        </Col>
+      </Row>
 </div>
   )
 }
--- src/Home/Product Card/ProductCard.jsx
+++ src/Home/Product Card/ProductCard.jsx
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import img from '../../images/anime-character-listening-music.jpg'
+import Popup from '../Popup';
 
 const ProductCard = ({img}) => {
+
+const [showPopup, setShowPopup] = useState(false);
+  const Pop=()=>{
+    setShowPopup(true);
+  }
+
   return (
+<>
     <div class="nft">
     <div class='main'>
 <img src={img.img} alt="" />
 <h3 className='creator'>{img.productName}</h3>( 10*5 inches ) <br/> Single pcs 
 <p><span style={{textDecoration:"line-through",color:"gray"}}>Rs.{img.oldPrice}</span>&nbsp;&nbsp;&nbsp;<span style={{fontSize:"22px"}}>Rs.{img.price}</span> </p>
 {/* <i class="fas fa-star" style={{color: "#FFD43B"}}></i> */}
-<button className='bt1'>Choose Options</button>
+<button className='bt1' onClick={Pop}>Choose Options</button>
 
 </div>
 </div>
+{showPopup===true&&
+(<Popup/>)}
+</>
   );
 }
 
