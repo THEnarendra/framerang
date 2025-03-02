3391a21fd458eda482c684902f1960890db854a7
niranjansharma01 <ns350099@gmail.com>
2025-03-02 19:41:17 +0530

    mnb

--- src/Header_Footer/Navbar.jsx
+++ src/Header_Footer/Navbar.jsx
@@ -8,16 +8,13 @@ import { useProducts } from "../Context/ProductContext";
 
 const Navbar = ({ theme, setTheme, setIsCartOpen }) => {
   const { cart } = useContext(CartContext);
-  const { categories, subcategories } = useProducts(); 
+  const { categories, subcategories } = useProducts();
   const [isOpen, setIsOpen] = useState(true);
   const [openCategory, setOpenCategory] = useState(null);
 
-  console.log("categories", categories);
-  console.log("subCategories", subcategories);
-  console.log("")
-
   const toggle = () => setIsOpen(!isOpen);
-  const ToggleTheme = () => setTheme(theme === "darkTheme" ? "lightTheme" : "darkTheme");
+  const ToggleTheme = () =>
+    setTheme(theme === "darkTheme" ? "lightTheme" : "darkTheme");
 
   return (
     <div>
@@ -28,66 +25,117 @@ const Navbar = ({ theme, setTheme, setIsCartOpen }) => {
           </Link>
         </Col>
 
-        <Col lg={3} md={8} sm={8}>
-          <input className="search" type="search" placeholder="Search Products" style={{ paddingLeft: 30 }} />
+        <Col lg={4} md={8} sm={8}>
+          <input
+            className="search"
+            type="search"
+            placeholder="Search Products"
+            style={{ paddingLeft: 30 }}
+          />
         </Col>
 
         <Col className={isOpen ? "navMenu" : "navItems"} lg={1} md={12}>
-          <Link to="/" className="link" style={{ color: theme === "darkTheme" ? "white" : "black" }}>
+          <Link
+            to="/"
+            className="link"
+            style={{ color: theme === "darkTheme" ? "white" : "black" }}
+          >
             Home
           </Link>
         </Col>
 
         <Col className={isOpen ? "navMenu" : "navItems"} lg={1} md={12}>
-        <div className="shop-section"
-                onMouseEnter={() => setOpenCategory("main")}
-                onMouseLeave={() => setOpenCategory(null)}>
-          <Link to="/collections" className="link" style={{ color: theme === "darkTheme" ? "white" : "black" }}>
-            <div>Shop</div>
-            {openCategory === "main" && (
+          <div
+            className="shop-section"
+            onMouseEnter={() => setOpenCategory("main")}
+            onMouseLeave={() => setOpenCategory(null)}
+          >
+            <Link
+              to="/collections"
+              className="link"
+              style={{ color: theme === "darkTheme" ? "white" : "black" }}
+            >
+              <div>Shop</div>
+              {openCategory === "main" && (
               <div className="dropdown-container">
                 {categories.map((category) => (
                   <div key={category.id} className="dropdown">
-                    <Link to={`/${category}`} className="link">
-                      {category.name}
+                    <Link
+                      to={`/${category}`}
+                      className="link text-white"
+                      // style={{
+                      //   color: theme === "darkTheme" ? "white" : "black",
+                      // }}
+                    >
+                      {category}
                     </Link>
                     <div className="dropdown-content">
-                    {subcategories[category] && subcategories[category].map((subcategory) => (
-                    <Link key={subcategory} to={`/${category}/${subcategory}`} className="link">
-                    {subcategory}
-                    </Link>
-                      ))}
+                      {subcategories[category] &&
+                        subcategories[category].map((subcategory) => (
+                          <Link
+                            key={subcategory}
+                            to={`/${category}/${subcategory}`}
+                            className="link text-white"
+                            // style={{
+                            //   color: theme === "darkTheme" ? "white" : "black",
+                            // }}
+                          >
+                            {subcategory}
+                          </Link>
+                        ))}
                     </div>
                   </div>
                 ))}
               </div>
-            )}
+             )}
             </Link>
-        </div>
+          </div>
         </Col>
 
-
         <Col className={isOpen ? "navMenu" : "navItems"} lg={1} md={12}>
-          <Link to="/contactus" className="link" style={{ color: theme === "darkTheme" ? "white" : "black" }}>
+          <Link
+            to="/contactus"
+            className="link"
+            style={{ color: theme === "darkTheme" ? "white" : "black" }}
+          >
             Contact
           </Link>
         </Col>
 
         <Col className={isOpen ? "navMenu" : "navItems"} lg={1} md={12}>
-          <Link to="/track" className="link" style={{ color: theme === "darkTheme" ? "white" : "black" }}>
+          <Link
+            to="/track"
+            className="link"
+            style={{ color: theme === "darkTheme" ? "white" : "black" }}
+          >
             Track
           </Link>
         </Col>
 
-        <Col className={isOpen ? "navMenu me-5" : "navItems"} lg={2} md={12}>
+        <Col
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "end",
+          }}
+          className={isOpen ? "navMenu" : "navItems"}
+          lg={1}
+        >
           <Link to="/notification" className="link">
             <i className="fa-solid fa-bell"></i>
           </Link>
           <span onClick={() => setIsCartOpen(true)} className="cart-badge">
             {cart?.length ? cart.length : ""}
           </span>
-          <i onClick={() => setIsCartOpen(true)} className="px-4 fa-solid fa-cart-shopping"></i>
-          <i onClick={ToggleTheme} className="ps-2 fa-solid fa-moon" style={{ cursor: "pointer" }}></i>
+          <i
+            onClick={() => setIsCartOpen(true)}
+            className="fa-solid fa-cart-shopping mt-1 px-3"
+          ></i>
+          <i
+            onClick={ToggleTheme}
+            className="fa-solid fa-moon mt-1"
+            style={{ cursor: "pointer" }}
+          ></i>
         </Col>
 
         <Col className="burger">
